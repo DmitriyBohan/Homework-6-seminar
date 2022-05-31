0ff6e0f1613f7515f5b1851c030a50d56685d4c0
Дмитрий бохан <manager25@office.net>
2022-05-31 21:17:24 +0300

    Homework-6-seminar

---
+++ TASK_45/Program.cs
@@ -0,0 +1,52 @@
+﻿/*
+Задача 45: 
+Напишите программу, которая будет создавать копию 
+заданного массива с помощью поэлементного копирования.
+*/
+
+int arrayLenght = new Random().Next(2, 30);
+
+void FillArray(int[] array1)
+{
+
+    for (int i = 0; i < arrayLenght; i++)
+    {
+        array1[i] = new Random().Next(0, 1000);
+    }
+
+}
+
+void CopyArray(int[] array2, int[] array3)
+{
+    for (int i = 0; i < arrayLenght; i++)
+    {
+        array3[i] = array2[i];
+    }
+}
+
+void PrintArray(int[] array4)
+{
+    Console.Write("[");
+    for (int i = 0; i < arrayLenght; i++)
+    {
+        Console.Write(array4[i]);
+        if (i != arrayLenght-1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+
+int [] array = new int [arrayLenght];
+int [] copyArray = new int [arrayLenght];
+
+FillArray(array);
+CopyArray(array,copyArray);
+Console.WriteLine("Оригинал массива");
+Console.WriteLine("-------------------------------------------------- ");
+PrintArray(array);
+Console.WriteLine("Копия массива");
+Console.WriteLine("-------------------------------------------------- ");
+PrintArray(copyArray);
---
+++ TASk_44/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Задача 44: 
+Не используя рекурсию, выведите первые 
+N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+Если N = 5 -> 0 1 1 2 3
+Если N = 3 -> 0 1 1
+Если N = 7 -> 0 1 1 2 3 5 8
+*/
+
+Console.WriteLine("Последовательность из скольки чисел Фибоначчи вы хотите получить?");
+int FibNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write($"N = {FibNumber} -> ");
+
+
+int prev = 0;
+int current = 1;
+int temp;
+
+Console.Write(prev + " ");
+for (int i = 0; i <= FibNumber - 2; i++)
+{
+    Console.Write(current + " ");
+    temp = current;
+    current = current + prev;
+    prev = temp;
+}
+
